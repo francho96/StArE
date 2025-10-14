5572c901c66b9994fe7b57786a0f8d2c9e780547
fbarria <fbarria@levelarriendos.cl>
2025-10-14 12:22:15 -0300

    ✨ metrics n await fix

--- Stare/stare.ts
+++ Stare/stare.ts
@@ -41,17 +41,17 @@ function ValidURL(str: string): boolean {
 }
 
 // THIS FUNCTION PREPARES THE JSON AND TRANSFORM IT TO STANDARD FORM
-const prepareSerp = function(serp_type: string, input: string | object): Promise<SerpObject> {
-    return new Promise(function(resolve, reject) {
+const prepareSerp = function (serp_type: string, input: string | object): Promise<SerpObject> {
+    return new Promise(function (resolve, reject) {
         if (typeof input === "string") {
             if (ValidURL(input)) {
                 serp_manager = require('./Serp_Process/' + serp_type);
                 serp_manager.pre_procesar(input)
-                    .then(function(json: SerpObject) {
+                    .then(function (json: SerpObject) {
                         json_object = json;
                         resolve(json_object);
                     })
-                    .catch(function(error: any) {
+                    .catch(function (error: any) {
                         reject(error);
                     });
             } else {
@@ -60,11 +60,11 @@ const prepareSerp = function(serp_type: string, input: string | object): Promise
         } else {
             serp_manager = require('./Serp_Process/' + serp_type);
             serp_manager.pre_procesar(input)
-                .then(function(json: SerpObject) {
+                .then(function (json: SerpObject) {
                     json_object = json;
                     resolve(json_object);
                 })
-                .catch(function(error: any) {
+                .catch(function (error: any) {
                     reject(error);
                 });
         }
@@ -72,41 +72,40 @@ const prepareSerp = function(serp_type: string, input: string | object): Promise
 };
 
 // STEP 2: DOWNLOAD DOCUMENTS
-const get_Document = function(num: number): Promise<number> {
-    return new Promise(function(resolve, reject) {
+const get_Document = function (num: number): Promise<number> {
+    return new Promise(function (resolve, reject) {
         if (!json_object || !json_object.documents || !json_object.documents[num]) {
             reject(new Error("Invalid document index or JSON object not initialized"));
             return;
         }
 
         web_scraper.get_HTML(json_object.documents[num].link, num, output)
-            .then(function(docNum: number) {
+            .then(function (docNum: number) {
                 return web_scraper.get_Doc(docNum, output);
             })
-            .then(function(docNum: number) {
-                console.log("Document " + docNum + " is downloaded!");
+            .then(function (docNum: number) {
                 resolve(docNum);
             })
-            .catch(function(error: any) {
+            .catch(function (error: any) {
                 reject(error);
             });
     });
 };
 
 // ONLY DOWNLOAD HTML's
-const get_HTML = function(num: number): Promise<number> {
-    return new Promise(function(resolve, reject) {
+const get_HTML = function (num: number): Promise<number> {
+    return new Promise(function (resolve, reject) {
         if (!json_object || !json_object.documents || !json_object.documents[num]) {
             reject(new Error("Invalid document index or JSON object not initialized"));
             return;
         }
 
         web_scraper.get_HTML(json_object.documents[num].link, num, output)
-            .then(function(docNum: number) {
+            .then(function (docNum: number) {
                 console.log("Document " + docNum + " is downloaded!");
                 resolve(docNum);
             })
-            .catch(function(error: any) {
+            .catch(function (error: any) {
                 reject(error);
             });
     });
@@ -119,25 +118,21 @@ let serp: boolean = false;
 let metric: Metric[] = [];
 
 // FUNCTION THAT GET THE METRICS
-const get_Metrics = function(...metricsNames: string[]): boolean {
-    console.log('tengo ' + metricsNames.length + ' metricas que calcular');
-    
+const get_Metrics = async function (...metricsNames: string[]): Promise<boolean> {
+    console.log('tengo ' + metricsNames.length + ' métricas que calcular');
+
     if (!json_object || !json_object.documents) {
         console.log("JSON object not initialized");
         return false;
     }
 
-    // Check if Documents or HTMLS are needed.
     metricsNames.forEach((metricName, i) => {
         try {
             metric[i] = require('./Metrics/' + metricName + '.ts') as Metric;
-            
-            if (metric[i].use_SERP())
-                serp = true;
-            if (metric[i].use_DOC())
-                documentNeeded = true;
-            if (metric[i].use_HTML())
-                html = true;
+
+            if (metric[i].use_SERP()) serp = true;
+            if (metric[i].use_DOC()) documentNeeded = true;
+            if (metric[i].use_HTML()) html = true;
 
             json_object!.documents.forEach(doc => {
                 doc[metricName] = 1;
@@ -147,127 +142,111 @@ const get_Metrics = function(...metricsNames: string[]): boolean {
         }
     });
 
-    // Get The Metrics
-
-    // CASE 1: ONLY USE THE SERP INFORMATION
+    // CASE 1: ONLY SERP
     if (!documentNeeded && !html) {
         console.log("No Download Needed");
-        
+
+        const promises: Promise<any>[] = [];
+
         metricsNames.forEach((metricName, j) => {
             if (metric[j]) {
                 json_object!.documents.forEach((doc, index) => {
-                    metric[j].get_value(json_object, index)
-                        .then(function(values: any[]) {
+                    const p = metric[j].get_value(json_object, index)
+                        .then(values => {
                             const nombre = values[1];
-                            json_object!.documents[values[0]][nombre] = values[2];
+                            json_object!.documents[values[2]][nombre] = values[0];
                         })
-                        .catch(function(error: any) {
-                            console.error("Error getting metric:", error);
-                        });
+                        .catch(err => console.error("Error getting metric:", err));
+
+                    promises.push(p);
                 });
             }
         });
+
+        await Promise.all(promises);
         return true;
     }
-    // CASE 2: ONLY NEEDS HTML INFORMATION
-    else if (html && !documentNeeded) {
+
+    // CASE 2: ONLY HTML INFO
+    if (html && !documentNeeded) {
         console.log("no cleaning needed");
-        
-        json_object.documents.forEach((doc, index) => {
-            get_HTML(index).then(
-                function(result: number) {
-                    if (result) {
-                        metricsNames.forEach((metricName, j) => {
-                            if (metric[j] && metric[j].use_HTML()) {
-                                metric[j].get_value(output + '/docs/HTML/HTML' + result + '.txt', result)
-                                    .then(function(values: any[]) {
-                                        json_object!.documents[values[2] as number][values[1] as string] = values[0];
-                                    })
-                                    .catch(function(error: any) {
-                                        console.error("Error getting HTML metric:", error);
-                                    });
-                            } else if (metric[j]) {
-                                metric[j].get_value(json_object, result)
-                                    .then(function(values: any[]) {
-                                        json_object!.documents[values[2] as number][values[1] as string] = values[0];
-                                    })
-                                    .catch(function(error: any) {
-                                        console.error("Error getting metric:", error);
-                                    });
-                            }
-                        });
+
+        const promises = json_object.documents.map(async (_, index) => {
+            try {
+                const result = await get_HTML(index);
+                if (!result) throw new Error("HTML fetch failed");
+
+                await Promise.all(metricsNames.map(async (metricName, j) => {
+                    if (!metric[j]) return;
+
+                    if (metric[j].use_HTML()) {
+                        const values = await metric[j].get_value(output + `/docs/HTML/HTML${result}.txt`, result);
+                        json_object!.documents[values[2]][values[1]] = values[0];
                     } else {
-                        console.log("Ups, Something Went Wrong");
+                        const values = await metric[j].get_value(json_object, result);
+                        json_object!.documents[values[2]][values[1]] = values[0];
                     }
-                }
-            ).catch(function(error: any) {
-                console.error("Error getting HTML:", error);
-            });
+                }));
+            } catch (err) {
+                console.error("Error processing HTML metric:", err);
+            }
         });
+
+        await Promise.all(promises);
         return true;
     }
-    // CASE 3: DOCUMENTS ARE NEEDED
-    else {
-        console.log("all is needed");
 
-        json_object.documents.forEach((doc, index) => {
-            get_Document(index).then(
-                function(result: number) {
-                    if (result) {
-                        console.log("html ready");
+    // ASE 3: DOCUMENTS ARE NEEDED
+    console.log("all is needed");
 
-                        metricsNames.forEach((metricName, x) => {
-                            if (metric[x]) {
-                                if (metric[x].use_DOC()) {
-                                    metric[x].get_value(output + '/docs/doc/doc' + result + '.txt', result)
-                                        .then(function(values: any[]) {
-                                            json_object!.documents[values[2] as number][values[1] as string] = values[0];
-                                        })
-                                        .catch(function(error: any) {
-                                            console.error("Error getting DOC metric:", error);
-                                        });
-                                } else if (metric[x].use_HTML()) {
-                                    metric[x].get_value(output + '/docs/HTML/HTML' + result + '.txt', result)
-                                        .then(function(values: any[]) {
-                                            json_object!.documents[values[2] as number][values[1] as string] = values[0];
-                                        })
-                                        .catch(function(error: any) {
-                                            console.error("Error getting HTML metric:", error);
-                                        });
-                                } else {
-                                    metric[x].get_value(json_object, result)
-                                        .then(function(values: any[]) {
-                                            json_object!.documents[values[2] as number][values[1] as string] = values[0];
-                                        })
-                                        .catch(function(error: any) {
-                                            console.error("Error getting metric:", error);
-                                        });
-                                }
-                            }
-                        });
+    const docPromises = json_object.documents.map(async (_, index) => {
+        try {
+            const result = await get_Document(index);
+            if (!result) throw new Error("Document fetch failed");
+
+            console.log("html ready");
+
+            await Promise.all(metricsNames.map(async (metricName, x) => {
+                if (!metric[x]) return;
+
+                try {
+                    let values;
+                    if (metric[x].use_DOC()) {
+                        values = await metric[x].get_value(output + `/docs/doc/doc${result}.txt`, result);
+                    } else if (metric[x].use_HTML()) {
+                        values = await metric[x].get_value(output + `/docs/HTML/HTML${result}.txt`, result);
                     } else {
-                        console.log("Ups, Something Went Wrong");
+                        values = await metric[x].get_value(json_object, result);
                     }
+
+                    json_object!.documents[values[2]][values[1]] = values[0];
+                } catch (err) {
+                    console.error(`Error processing metric ${metricName} for document ${result}:`, err);
+                    json_object!.documents[result][metricName] = null;
                 }
-            ).catch(function(error: any) {
-                console.error("Error getting document:", error);
-            });
-        });
-        return true;
-    }
+            }));
+
+        } catch (err) {
+            console.error("Error processing DOC metric:", err);
+        }
+    });
+
+    await Promise.all(docPromises);
+    return true;
 };
 
+
 // GET NUMBER OF ITEMS
-const get_Items = function(): number {
+const get_Items = function (): number {
     return json_object ? json_object.items : 0;
 };
 
 // GET JSON
-const get_Json = function(): string {
+const get_Json = function (): string {
     return json_object ? JSON.stringify(json_object) : "{}";
 };
 
-const reset = function(): void {
+const reset = function (): void {
     json_object = null;
 };
 
