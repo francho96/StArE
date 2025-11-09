e077e63d0c6f2af1abf5942121c838a06f6f91fc
francho96 <franco.barria@usach.cl>
2025-11-09 14:12:52 -0300

    ⚗️ added env on client

--- client/src/calls/google-fetch.ts
+++ client/src/calls/google-fetch.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 
 export const fetchDocuments = async (string: string) => {
-    const link = `http://localhost:3000/google?q=${string}&p=0` 
+    const link = `${import.meta.env.VITE_BACKEND}/google?q=${string}&p=0` 
 
     try {
         const {data} = await axios(link)
